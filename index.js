d7e4bf380c96e49bb4b7786511d3146074d0f492
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-05-08 01:07:43 -0700

    added dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const server = require('./server.js');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 server.listen(port, () => {
   console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
